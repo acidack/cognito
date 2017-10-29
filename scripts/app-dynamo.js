d7406e7e4e2d9587eee2d49c384c66711047a93e
Patel <patesumi@f45c899ace0f.ant.amazon.com>
2017-10-29 22:31:56 +1100

    first commit

--- scripts/app-dynamo.js
+++ scripts/app-dynamo.js
@@ -1,6 +1,6 @@
 var poolData = {
-    UserPoolId : 'ap-southeast-1_3dbj6x7Wo',
-    ClientId : '7oa4h8g7m6lj3h1jv7obriof6m'
+    UserPoolId : 'us-east-1_dFJSHbGDU',
+    ClientId : '1qesc53g3omgbmf5al4frnucku'
 };
 
 var userPool = new AWSCognito.CognitoIdentityServiceProvider.CognitoUserPool(poolData);
